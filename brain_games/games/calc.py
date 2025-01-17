c3e2bd9e53f18cef08d22e79cc31698e801afaf9
bolnitsa <garmashov3@mail.ru>
2025-01-17 19:56:12 +0300

    brain_games/games/calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,10 +1,18 @@
 import random
 
-
 GAME_RULES = 'What is the result of the expression?'
 MIN_NUM = 1
 MAX_NUM = 100
 
+def calculate(left_num, operator, right_num):
+    if operator == '+':
+        return left_num + right_num
+    elif operator == '-':
+        return left_num - right_num
+    elif operator == '*':
+        return left_num * right_num
+    else:
+        raise ValueError("Unknown operator: {}".format(operator))
 
 def play():
     operators = ['+', '-', '*']
@@ -13,7 +21,7 @@ def play():
     left_num = random.randint(MIN_NUM, MAX_NUM)
     right_num = random.randint(MIN_NUM, MAX_NUM)
 
-    expression = '{} {} {}'.format(left_num, operator, right_num)
-    correct_answer = str(eval(expression))
+    expression = f'{left_num} {operator} {right_num}'
+    correct_answer = str(calculate(left_num, operator, right_num))
 
     return expression, correct_answer
